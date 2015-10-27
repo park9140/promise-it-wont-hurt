de7ed7d2f781b7091fa8a8ef8a08386e902d05f9
Jonathan Park <jonathan.park@changepoint.com>
2015-10-27 12:41:08 -0700

    fix a few bugs

--- exercises/fulfill_a_promise/solution/solution.js
+++ exercises/fulfill_a_promise/solution/solution.js
@@ -1,4 +1,4 @@
 var promise = new Promise(function(resolve) {
     setTimeout(resolve, 300, "RESOLVED!");
-}
+});
 promise.then(console.log);
--- exercises/reject_a_promise/solution/solution.js
+++ exercises/reject_a_promise/solution/solution.js
@@ -1,6 +1,6 @@
-var promise = new Promise(function(resolve) {
+var promise = new Promise(function(resolve, reject) {
     setTimeout(reject, 300, new Error("REJECTED!"));
-}
+});
 
 function printError (err) {
   console.log(err.message);
